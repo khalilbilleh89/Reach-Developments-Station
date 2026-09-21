7b9c1494e4d2781e966a080c4391c65039a862d5
Youssef Awwad <youssefawwad88@gmail.com>
2026-09-21 12:12:15 +0300

    Keep sales merge revision within Alembic version limit

--- app/db/migrations/versions/0037_merge_sales.py
+++ app/db/migrations/versions/0037_merge_sales.py
@@ -1,6 +1,6 @@
 """Merge the independent Agent registry and buyer operations histories."""
 
-revision = "0037_merge_sales_agents_operations"
+revision = "0037_merge_sales"
 down_revision = ("0036_sales_agents", "0036_sales_operations")
 branch_labels = None
 depends_on = None
